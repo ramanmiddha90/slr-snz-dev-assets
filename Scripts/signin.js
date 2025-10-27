1fe63d1e33be74ea4a8ed117794ff14924c24d41
ramanmiddha90 <middha.raman@gmail.com>
2025-10-27 13:54:34 +0530

    refactored sign in js

--- Scripts/signin.js
+++ Scripts/signin.js
@@ -6,39 +6,12 @@
                 var policyPrefix = "B2C_1A_SLR_SNZ";
                 var SNZPolicy = "SNZ_LOGIN";
                 var SwissRXPolicy = "SWISSRX_LOGIN";
-                var SetInvitationElements = function () {
-
-                    $("#signInName").on("change paste keyup", function () {
-                        //console.log($(this).val());
-                        // $("#CurrentSignInStatus").val("NF");
-                        // $("#invitationCode").hide();
-                    });
-
+                var HandleSpinnerEvents = function () {
                     var targetNode = document.getElementById('claimVerificationServerError');
                     //debugger;
-                    $("#divInvitationCode").hide();
                     var observer = new MutationObserver(function () {
                         if (targetNode.style.display != 'none') {
-                            //debugger;
-                            /*   console.log("claimVerificationServerError");*/
                             LoadSpinner(false);
-                            if ($("#claimVerificationServerError").text().includes("username or password provided")) {
-                               
-                                $("#claimVerificationServerError").text(GetTranslationBasedOnCode("S-001"));
-                            }
-                            var errorCode = GetCancelCodeBasedOnMessage();
-                            $("#attributeList").after($("#claimVerificationServerError"))
-                            if (errorCode == "461") {
-                                $("#invitationCode").val("");
-                                $("#invitationCode").show();
-                                //$("#claimVerificationServerError").after($("#invitationCode"));
-                                $("#CurrentSignInStatus").val("AI");
-                            }
-
-                        }
-                        else {
-                            /* $("#CurrentSignInStatus").val("NF");*/
-                            //$("#invitationCode").hide();
                         }
                     });
                     observer.observe(targetNode, { attributes: true, attributeFilter: ['style'] });
@@ -51,63 +24,6 @@
                         document.getElementById('loader-backdrop').style.display = 'none';
                     }
                 }
-                function SetUILabels() {
-                    try {
-                        if ($("#ErrorMappings") != null) {
-
-                            var UI_Locales = $.parseJSON($("#ErrorMappings").val());
-                            UI_Locales.StatusMapping.forEach(function (UIElementConfig) {
-                                if (UIElementConfig.Type && UIElementConfig.Type == "label") {
-                                    //code is id
-                                    var currentElement = $("#" + UIElementConfig.Code);
-                                    if (currentElement) {
-                                        currentElement.html(UIElementConfig.Text);
-                                    }
-
-                                }
-                            });
-                        }
-                    }
-                    catch {
-
-                    }
-                }
-                function GetTranslationBasedOnCode(code) {
-                    var message = $("#claimVerificationServerError")?.text();
-                    if ($("#ErrorMappings") != null) {
-
-                        var UI_Locales = $.parseJSON($("#ErrorMappings").val());
-
-                        UI_Locales.StatusMapping.forEach(function (error) {
-
-                            if (error.Code != undefined && error.Code == code) {
-                                message = error.Text;
-                                return false;
-                            }
-                        });
-
-                    }
-                    return message;
-                };
-                function GetCancelCodeBasedOnMessage() {
-                    var errorCode = 999;
-                    if ($("#ErrorMappings") != null) {
-
-                        var UI_Locales = $.parseJSON($("#ErrorMappings").val());
-                        var currentErrorMessage = $("#claimVerificationServerError")?.text();
-                        if (currentErrorMessage) {
-
-                            UI_Locales.StatusMapping.forEach(function (error) {
-
-                                if (error.Message != undefined && error.Message == currentErrorMessage) {
-                                    errorCode = error.Code;
-                                    return false;
-                                }
-                            });
-                        }
-                    }
-                    return errorCode;
-                };
                 function GetParameterValues(param) {
                     var url = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
                     for (var i = 0; i < url.length; i++) {
@@ -131,12 +47,10 @@
                     var countryCode = setQueryParam(queryparams.countryCode, "");
                     var applicationType = setQueryParam(queryparams.applicationType, "MIAC");
                     var return_url = queryparams.return_url ?? "";
-                    var regType = setQueryParam(queryparams.regType, "V1");
                     var clientId = queryparams.clientId ?? "";
                     var redirect_uri = queryparams.redirect_uri ?? "";
                     var UILanguage = queryparams.UILanguage ?? "EN";
                     var queryparams = new URLSearchParams(window.location.search);
-
                     var originURL = document.domain;
                     var tenantName = originURL.replace(".b2clogin.com", "") + ".onmicrosoft.com";
                     var redirectURL = "https://" + originURL + "/" + tenantName +
@@ -165,10 +79,6 @@
                     });
 
                     $("#signup").click(function (event) {
-
-                        //var queryparams = new URLSearchParams(window.location.search);
-                        //queryparams.set("p", "B2C_1A_SOLAR_SANDOZID_PROD_SIGNUP");
-                        //window.location.search = queryparams.toString();
                         window.location = ConfigureRedirectURL("B2C_1A_SLR_SNZ_SIGNUP");
                     });
                 };
@@ -221,7 +131,7 @@
                 var setCustomLabels = function () {
                     if ($("#api"))
                         $("#api > .intro:eq(0) ").before("<div class='pageheader intropageheader intro'><p id='intropageheader_lbl'>Login</p></div>");
-                    SetUILabels();
+                    
                     // SetIDPs();
                     $("#customCancel").text($("#cancel").text())
 
@@ -230,7 +140,6 @@
                 var continuteButton = document.getElementById('continue');
                 if (continuteButton && $("#continue").is(':visible')) {
 
-                    //AddPasswordResetLink();
                     if ($("#CurrentSignInStatus")) {
                         $("#CurrentSignInStatus").val("NF");
                     }
@@ -245,20 +154,20 @@
 
                     $("#customCancel").click(function () {
                         //debugger;
-                        var errorCode = GetCancelCodeBasedOnMessage();
+                      
                         var redirectURL = GetParameterValues('return_url'); //Encoded value FE URL
                         if (redirectURL == null)
                             redirectURL = "";
                         //microsoft redirecturl
                         var redirectURI = GetParameterValues('redirect_uri');
                         //var url = decodeURIComponent(redirectURI) + "#error=" + errorCode + ":" + redirectURL;
-                        var url = decodeURIComponent(redirectURI) + "#error=access_denied&error_description=AAD_Custom_" + errorCode + ":" + redirectURL;
+                        var url = decodeURIComponent(redirectURI) + "#error=access_denied&error_description=AAD_Custom_461:" + redirectURL;
                         window.location.replace(url);
 
 
                     });
 
-                    // SetInvitationElements();
+                    HandleSpinnerEvents();
                     BindEvent();
                     clearInterval(intervalHandle);
 
