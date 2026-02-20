3600e52b7931d0982b5301e482443ba1d622b7fa
ramanmiddha90 <middha.raman@gmail.com>
2026-02-20 12:44:55 +0530

    sl change for profile eidt

--- Scripts/profileedit.js
+++ Scripts/profileedit.js
@@ -103,7 +103,7 @@
     const ASSETS = Object.freeze({
         success: "https://slr-snz-dev-assets.pages.dev/objects/success.svg",
         failed: "https://slr-snz-dev-assets.pages.dev/objects/failed.svg",
-        sl_logo: "https://slr-snz-dev-assets.pages.dev/objects/sandoz_logo_sl.svg"
+        sl_logo: "https://slr-snz-dev-assets.pages.dev/objects/sandoz_logo_sl_1.svg"
     });
     // ==========================
     // Validation
